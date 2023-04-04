d0d7f61079fa328c4077ed79a00e35d001732f89
Dinesh <dineshk@adobe.com>
2023-04-04 12:50:20 +0530

    Sling Models

--- On-permis/dinesh-learning/core/src/main/java/dinesh/learning/core/models/HelloWorldModel.java
+++ On-permis/dinesh-learning/core/src/main/java/dinesh/learning/core/models/HelloWorldModel.java
@@ -15,35 +15,67 @@
  */
 package dinesh.learning.core.models;
 
-import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
-
-import javax.annotation.PostConstruct;
-
+import com.adobe.cq.export.json.SlingModelFilter;
+import com.day.cq.commons.jcr.JcrConstants;
+import com.day.cq.search.QueryBuilder;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
-import org.apache.sling.models.annotations.injectorspecific.OSGiService;
-import org.apache.sling.models.annotations.injectorspecific.SlingObject;
-import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.apache.sling.models.annotations.injectorspecific.*;
 
-import com.day.cq.wcm.api.Page;
-import com.day.cq.wcm.api.PageManager;
-
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
 import java.util.Optional;
 
-@Model(adaptables = Resource.class)
+import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
+import static org.apache.sling.models.annotations.DefaultInjectionStrategy.OPTIONAL;
+
+@Model(adaptables = {Resource.class, SlingHttpServletRequest.class}, defaultInjectionStrategy =  OPTIONAL)
 public class HelloWorldModel {
+
+    /********* SlingHttpServletRequest.class **************/
+
+    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL,name = JcrConstants.JCR_TITLE)
+    private String pageTitle;
+
+    @ValueMapValue
+    @Named("jcr:title")
+    private String pageTitlee;
+
+    @ScriptVariable(name = JcrConstants.JCR_TITLE)
+    private String pageTitleee;
+
+    @ScriptVariable
+    private Page currentPage;
+
+    @ScriptVariable
+    Resource resource;
+
+    /********* Resource.class ***********/
 
     @ValueMapValue(name=PROPERTY_RESOURCE_TYPE, injectionStrategy=InjectionStrategy.OPTIONAL)
     @Default(values="No resourceType")
     protected String resourceType;
-
     @SlingObject
     private Resource currentResource;
     @SlingObject
     private ResourceResolver resourceResolver;
+
+    /******* Calling to Another OSGi service ********/
+
+    @OSGiService
+    private QueryBuilder queryBuilder;
+
+    @OSGiService
+    private SlingModelFilter slingModelFilter;
+
+
+   /******* Variable Declarations **********/
+
 
     private String message;
 
@@ -57,10 +89,15 @@ public class HelloWorldModel {
         message = "Hello World!\n"
             + "Resource type is: " + resourceType + "\n"
             + "Current page is:  " + currentPagePath + "\n";
+
+        System.out.println("Current Page " + currentPage);
+        System.out.println("Page Tile " + pageTitle);
+        System.out.println("Resource " + resource);
     }
 
     public String getMessage() {
         return message;
     }
+
 
 }
